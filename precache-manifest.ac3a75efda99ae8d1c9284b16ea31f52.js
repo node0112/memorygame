301a1d6197164089a822e24ed6e39fe95efb6d4c
node0112 <siddheshthorat25@gmail.com>
2022-07-23 11:56:22 +0530

    Updates

---
+++ precache-manifest.ac3a75efda99ae8d1c9284b16ea31f52.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "42983ba598cf368914cf",
+    "url": "/memorygame/static/css/main.43baacde.chunk.css"
+  },
+  {
+    "revision": "42983ba598cf368914cf",
+    "url": "/memorygame/static/js/main.42983ba5.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/memorygame/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "7a8d3e99d1052ce3bf16",
+    "url": "/memorygame/static/js/2.7a8d3e99.chunk.js"
+  },
+  {
+    "revision": "764bd5d510d8af59e86f",
+    "url": "/memorygame/static/js/runtime~main.764bd5d5.js"
+  },
+  {
+    "revision": "9d6615b141ba7c64b3fa05e5689ccc88",
+    "url": "/memorygame/static/media/mordecai.9d6615b1.png"
+  },
+  {
+    "revision": "f24958af51eb08e9dd115e376a58bcf9",
+    "url": "/memorygame/static/media/eileen.f24958af.png"
+  },
+  {
+    "revision": "ca5fd05d08459809c9a529e708903377",
+    "url": "/memorygame/static/media/margaret.ca5fd05d.png"
+  },
+  {
+    "revision": "7aaa22f8c333830d0f4b5639a92d9b5c",
+    "url": "/memorygame/static/media/muscleman.7aaa22f8.png"
+  },
+  {
+    "revision": "b860508e74d9fbd721f9f36859648298",
+    "url": "/memorygame/static/media/rigby.b860508e.png"
+  },
+  {
+    "revision": "a17e16ed0565720d085e8b7a91556f95",
+    "url": "/memorygame/static/media/skips.a17e16ed.png"
+  },
+  {
+    "revision": "55cf0e3d7e9607d51ee03cd149708985",
+    "url": "/memorygame/static/media/thomas.55cf0e3d.png"
+  },
+  {
+    "revision": "d8b3e0fd853fa5416a3b979731f53b7a",
+    "url": "/memorygame/static/media/benson.d8b3e0fd.png"
+  },
+  {
+    "revision": "5ce25af3745d59fab476586650ae70e2",
+    "url": "/memorygame/static/media/hero_main.5ce25af3.png"
+  },
+  {
+    "revision": "9fb719acd66a7d1e2261daa69ad8b5f3",
+    "url": "/memorygame/index.html"
+  }
+];
